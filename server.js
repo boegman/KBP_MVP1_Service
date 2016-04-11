ed03eebb3f2cdb80e31b91dc1122fc9b62c64ce7
Gean Boegman <GeanB@Ovationsgroup.com>
2016-04-11 14:47:10 +0200

    forgot these two files in the compare with ordercloud project

--- server.js
+++ server.js
@@ -6,11 +6,23 @@ log4js.configure('log4js.json', { reloadSecs: 60 }); // the reload defaults to a
 var logger = log4js.getLogger('server');
 
 // modules =================================================
+var express = require('express');
+var http = require('http');
+var https = require('https');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var nconf = require('nconf');
+var compression = require('compression');
 
 // configuration ===========================================
 nconf.argv().env();
 nconf.file({ file: path.join(process.cwd(), '/config/env/', (process.env.NODE_ENV || 'dev') + '/settings.json') });
+var enableSsl = nconf.get("enableSsl");
+// Creating our server =====================================
+var app = express();
+var server = !enableSsl ? http.createServer(app) : https.createServer(options, app);
+
 // further module loading===================================
 var routeConfig = require('./app/routes');
 
@@ -19,8 +31,8 @@ var port = process.env.PORT || 3000;
 var defaultPage = 'index.html';
 
 app.use(compression());
-app.engine('.hbs', handlebars({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
+//app.engine('.hbs', handlebars({ extname: '.hbs' }));
+//app.set('view engine', '.hbs');
 app.use(cors());
 
 // get all data/stuff of the body (POST) parameters
@@ -32,7 +44,7 @@ app.use(bodyParser.text({ defaultCharset: 'ISO-8859-1' }));
 app.use(bodyParser.text());
 
 // routes ==================================================
-routeConfig.register(app, io); // configure our routes
+routeConfig.register(app/*, io*/); // configure our routes
 // start app ===============================================
 // startup our app at http://localhost:1337
 logger.debug('Trying to host on port: ' + port);
@@ -40,5 +52,5 @@ logger.debug('Trying to host on port: ' + port);
 
 server.listen(port, function () {
     // shoutout to the user          
-    logger.info('OvaPay listening on port ' + port);           
+    logger.info('Service listening on port ' + port);           
 });
